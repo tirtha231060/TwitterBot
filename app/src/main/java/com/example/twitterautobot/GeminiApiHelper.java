8f53d456f0ab58b0b89376be391f3fdc89794b06
tirtha231060 <tirtharoyvuson@gmail.com>
2026-05-26 20:03:24 +0600

    Add GeminiApiHelper class with getReply method

---
+++ app/src/main/java/com/example/twitterautobot/GeminiApiHelper.java
@@ -0,0 +1,8 @@
+package com.example.twitterautobot;
+
+public class GeminiApiHelper {
+    public static String getReply(String tweetText, String apiKey) {
+        // API কল করে রিপ্লাই আনার লজিক
+        return "Nice Tweet!"; 
+    }
+}
